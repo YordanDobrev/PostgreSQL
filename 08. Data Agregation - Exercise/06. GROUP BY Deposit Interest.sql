1afafae84ac6d87bb8c02674148364b814c5301d
YordanDobrev <petolisko@gmail.com>
2024-05-27 18:13:39 +0300

    Task completed

---
+++ 08. Data Agregation - Exercise/06. GROUP BY Deposit Interest.sql
@@ -0,0 +1,6 @@
+SELECT
+	deposit_group,
+	SUM(deposit_interest) as deposit_interest
+FROM wizard_deposits
+GROUP BY deposit_group
+ORDER BY deposit_interest DESC
---
+++ 08. Data Agregation - Exercise/09. Notes with Dumbledore.sql
@@ -0,0 +1,6 @@
+SELECT
+	last_name,
+	COUNT(notes) as notes_with_dumbledore
+FROM wizard_deposits
+WHERE notes LIKE '%Dumbledore%'
+GROUP BY last_name
